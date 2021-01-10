1dec524b817beee2609fac1f7e14d2503b05bb51
Wendell Priebe <wendel_oliveira94@hotmail.com>
2021-01-09 21:36:19 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+            Console.WriteLine($"Hello World! {i}");
+            }
+        }
+    }
+}
